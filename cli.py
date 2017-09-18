c67f42ae9705d88e7ff82ba2f5ceb1f5c54d0036
Huang Zeyu <sam.zyhuang@outlook.com>
2017-09-18 14:27:04 +0800

    Minor change on the interface

--- cli.py
+++ cli.py
@@ -27,7 +27,7 @@ while True:
     click.clear()
 
     while True:
-        course = click.prompt('Enter a course code\n(Case insensitive)')
+        course = click.prompt('Enter a course code (Case insensitive)').upper()
         click.echo('\nAcquiring course page...\n')
         try:
             overall_soup = ustreview.request_review_page_soup(course)
@@ -37,12 +37,12 @@ while True:
             break
     course_rating = ustreview.get_course_rating(overall_soup)
     click.echo((""
-                "Rating of this course:"
+                "Rating of {course}:"
                 "Content:\t{0}"
                 "Teaching:\t{1}"
                 "Grading:\t{2}"
                 "Workload:\t{3}"
-                "").format(*course_rating))
+                "").format(*course_rating, course=course))
     click.echo('Found reviews of the following instructors:')
     instructors = ustreview.get_instructor_list(overall_soup)
     for i, name in enumerate(instructors):
@@ -61,12 +61,12 @@ while True:
                 click.echo('Invalid number.')
             else:
                 break
-            instructor_no = click.prompt('Enter a instructor\'s No.')
+            instructor_no = click.prompt('Enter a instructor\'s No. Enter \'back\' to go back.')
 
         instructor_soup = ustreview.request_instructor_page_soup(selected_instructor)
         ratings = ustreview.get_instructor_review(instructor_soup)
         click.echo((""
-                    "Average rating of this instructor:"
+                    "Average rating of {instructor}:"
                     "Content:\t{0[0]} ({1[0]})"
                     "Teaching:\t{0[1]} ({1[1]})"
                     "Grading:\t{0[2]} ({1[2]})"
@@ -76,5 +76,5 @@ while True:
                     "Teaching:\t{2[1]}"
                     "Grading:\t{2[2]}"
                     "Workload:\t{2[3]}"
-                    "").format(*ratings))
-        instructor_no = click.prompt('Enter a instructor\'s No.')
+                    "").format(*ratings, instructor=selected_instructor))
+        instructor_no = click.prompt('Enter a instructor\'s No. Enter \'back\' to go back.')
