d069a0132c2e5b181a1c3735b7eb6b6bc294897d
Huang Zeyu <sam.zyhuang@outlook.com>
2017-09-19 16:09:24 +0800

    Interface and readme completion

--- README.md
+++ README.md
@@ -8,13 +8,39 @@ A simple spider used to enquire about the reviews of professors in HKUST. Data i
 
 ## 开发环境 Requirement
 
-Python 3.6 (2 is probably also okay)
+Python 3.5
 
 `pip install selenium`
 
 `pip install bs4`
 
-## TODOs
+如果需要运行程序，请确保已经下载了至少一个WebDriver且加入了环境变量。程序调用WebDriver的规则如下：
+
+If you wish to run the program on your computer, please ensure that at least one WebDriver has been downloaded and added to `PATH`. The rule it follows to choose WebDriver is shown below:
+
+| First flag              | WebDriver |
+| ----------------------- | --------- |
+| `--debug` or `--chrome` | Chrome    |
+| `--firefox`             | Firefox   |
+| Other or none           | PhantomJS |
+
+
+
+
+
+若要打包分发，则还需要
+
+In case of distribution, you may also need
+
+`pip install pyinstaller`
+
+只需执行
+
+You only need to run command
+
+`pyinstaller cli.py`
+
+## TODO
 
 * 访问过于频繁会触发reCaptcha
 * 解析js以代替Selenium提高速度
@@ -29,3 +55,11 @@ Python 3.6 (2 is probably also okay)
 ## 许可 License
 
 ![WTFPL](http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png)
+
+软件图标修改自作品[Professor by Saeful Muslim from the Noun Project](https://thenounproject.com/term/professor/1171625)。原作品采用[知识共享署名 3.0 美国许可协议](https://creativecommons.org/licenses/by/3.0/us/deed.zh)发布。
+
+The software icon is derived from [Professor by Saeful Muslim from the Noun Project](https://thenounproject.com/term/professor/1171625). The original work is licensed under [Creative Commons Attribution 3.0 United States License](http://creativecommons.org/licenses/by/3.0/us/)
+
+本软件作为个人制作的第三方软件，并非附属于香港科技大学、[UST Space](https://ust.space)网站或者任何其它组织、团队。
+
+This software is a third-party software, and is not affiliated to HKUST, [UST Space](https://ust.space) website or any other organization or group.
--- cli.py
+++ cli.py
@@ -11,6 +11,39 @@ import spider
 
 atexit.register(spider.quit)
 
+def formatted_course_info(name, ratings):
+    return '\n'.join(("",
+                "Rating of {course}:",
+                "Content:\t{0}",
+                "Teaching:\t{1}",
+                "Grading:\t{2}",
+                "Workload:\t{3}",
+                "")).format(*ratings, course=name)
+
+def formatted_instructor_info(name, ratings):
+    return '\n'.join(("",
+                "Average rating of {instructor}:",
+                "Content:\t{0[0]} ({1[0]})",
+                "Teaching:\t{0[1]} ({1[1]})",
+                "Grading:\t{0[2]} ({1[2]})",
+                "Workload:\t{0[3]} ({1[3]})",
+                "Variance of the ratings (the smaller the better):",
+                "Content:\t{2[0]}",
+                "Teaching:\t{2[1]}",
+                "Grading:\t{2[2]}",
+                "Workload:\t{2[3]}",
+                "")).format(*ratings, instructor=name)
+
+def formatted_limited_info(name, data):
+    return '\n'.join(("",
+                "Accessible rating of {course}:",
+                "Instructor:\t{2}"
+                "Content:\t{0[0]} ({1[0]})",
+                "Teaching:\t{0[1]} ({1[1]})",
+                "Grading:\t{0[2]} ({1[2]})",
+                "Workload:\t{0[3]} ({1[3]})",
+                "")).format(*data, course=name)
+
 while True:
     click.clear()
     username = click.prompt("Enter your username")
@@ -30,51 +63,39 @@ while True:
         course = click.prompt("Enter a course code (Case insensitive)").upper()
         click.echo("\nAcquiring course page...\n")
         try:
-            overall_soup = spider.request_review_page_soup(course)
+            overall_soup, has_full_access = spider.request_review_page_soup(course)
         except ValueError as e:
             click.echo(e)
         else:
             break
     course_rating = spider.get_course_rating(overall_soup)
-    click.echo('\n'.join(("",
-                "Rating of {course}:",
-                "Content:\t{0}",
-                "Teaching:\t{1}",
-                "Grading:\t{2}",
-                "Workload:\t{3}",
-                "")).format(*course_rating, course=course))
-    click.echo("Found reviews of the following instructors:")
-    instructors = spider.get_instructor_list(overall_soup)
-    for i, name in enumerate(instructors):
-        click.echo("{0:<2}: {1}".format(i, name))
+    click.echo(formatted_course_info(course, course_rating))
+
+    if has_full_access:
+        click.echo("Found reviews of the following instructors:")
+        instructors = spider.get_instructor_list(overall_soup)
+        for i, name in enumerate(instructors):
+            click.echo("{0:<2}: {1}".format(i, name))
 
 
-    instructor_no = click.prompt("Enter a instructor's No. Enter 'back' to go back.").lower()
-    while instructor_no != "back":
-        while True:
-            try:
-                instructor_no = int(instructor_no)
-                selected_instructor = instructors[instructor_no]
-            except ValueError:
-                click.echo("This is not a number.")
-            except IndexError:
-                click.echo("Invalid number.")
-            else:
-                break
+        instructor_no = click.prompt("Enter a instructor's No. Enter 'back' to go back.").lower()
+        while instructor_no != "back":
+            while True:
+                try:
+                    instructor_no = int(instructor_no)
+                    selected_instructor = instructors[instructor_no]
+                except ValueError:
+                    click.echo("This is not a number.")
+                except IndexError:
+                    click.echo("Invalid number.")
+                else:
+                    break
+                instructor_no = click.prompt("Enter a instructor's No. Enter 'back' to go back.")
+
+            instructor_soup = spider.request_instructor_page_soup(selected_instructor)
+            ratings_data = spider.get_instructor_review(instructor_soup)
+            click.echo(formatted_instructor_info(selected_instructor, ratings_data))
             instructor_no = click.prompt("Enter a instructor's No. Enter 'back' to go back.")
-
-        instructor_soup = spider.request_instructor_page_soup(selected_instructor)
-        ratings = spider.get_instructor_review(instructor_soup)
-        click.echo('\n'.join(("",
-                    "Average rating of {instructor}:",
-                    "Content:\t{0[0]} ({1[0]})",
-                    "Teaching:\t{0[1]} ({1[1]})",
-                    "Grading:\t{0[2]} ({1[2]})",
-                    "Workload:\t{0[3]} ({1[3]})",
-                    "Variance of the ratings (the smaller the better):",
-                    "Content:\t{2[0]}",
-                    "Teaching:\t{2[1]}",
-                    "Grading:\t{2[2]}",
-                    "Workload:\t{2[3]}",
-                    "")).format(*ratings, instructor=selected_instructor))
-        instructor_no = click.prompt("Enter a instructor's No. Enter 'back' to go back.")
+    else:
+        click.echo('You can only read the latest review of this course.')
+        ratings_data = spider.get_limited_review(overall_soup)
